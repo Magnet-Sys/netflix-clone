587a289df5cc9975c17e1da987667feb0610cc03
Magnet-Sys <80355938+Magnet-Sys@users.noreply.github.com>
2025-02-19 18:36:46 -0300

    added auth routes

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
 import express from "express"; // es6
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
-app.get("/signup", (req, res) => {
-  res.send("Signup route");
-});
+// app.get("/api/v1/signup", (req, res) => {
+//   res.send("Signup route");
+// });
 
-app.get("/login", (req, res) => {
-  res.send("Login route");
-});
+// app.get("/api/v1/login", (req, res) => {
+//   res.send("Login route");
+// });
 
-app.get("/logout", (req, res) => {
-  res.send("Logout route");
-});
+// app.get("/api/v1/logout", (req, res) => {
+//   res.send("Logout route");
+// });
+
+// ("api/v1/signup");
+
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
