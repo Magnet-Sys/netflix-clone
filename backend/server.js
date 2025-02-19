84ddb4853cdba3ff672f3893e9039cc10579b37f
Magnet-Sys <80355938+Magnet-Sys@users.noreply.github.com>
2025-02-19 18:22:50 -0300

    routes (endpoints) are defined

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
-// const express = require("express"); // common js
-
 import express from "express"; // es6
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/signup", (req, res) => {
+  res.send("Signup route");
+});
+
+app.get("/login", (req, res) => {
+  res.send("Login route");
+});
+
+app.get("/logout", (req, res) => {
+  res.send("Logout route");
 });
 
 app.listen(5000, () => {
