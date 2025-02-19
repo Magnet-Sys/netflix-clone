31754e065bdd6c25046e81d56df682bf3febf7f9
Magnet-Sys <80355938+Magnet-Sys@users.noreply.github.com>
2025-02-19 18:05:03 -0300

    http://localhost:5000 working fine

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ import express from "express"; // es6
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
