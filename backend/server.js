4c956dd74dae3accd125dd931a8df4184bad80c3
Magnet-Sys <80355938+Magnet-Sys@users.noreply.github.com>
2025-02-19 18:02:44 -0300

    test npm run dev

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
-const express = require("express"); // common js
+// const express = require("express"); // common js
 
 import express from "express"; // es6
+
+const app = express();
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
